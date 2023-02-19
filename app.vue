571330c345a561e5cdd78667249240a122f9d60b
Raymond Yeh <raymond.yeh@milkfish.digital>
2023-02-19 21:13:53 +1100

    feat(chore): Init nuxt with Tailwind

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-4xl font-bold ">Hello World</h1>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: ["@/assets/css/main.css"],
+  typescript: {
+    shim: false,
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
