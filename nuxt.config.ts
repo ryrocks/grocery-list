5841bffe4c0ea5d81bdcde26a31d93fb657a87c5
Raymond Yeh <raymond.yeh@milkfish.digital>
2023-02-21 07:58:51 +1100

    chore: Add title and meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: "utf-8",
+      title: "Grocery List",
+      meta: [{ name: "description", content: "Vue assessment" }],
+    },
+  },
   css: ["@/assets/css/main.css"],
   typescript: {
     shim: false,
